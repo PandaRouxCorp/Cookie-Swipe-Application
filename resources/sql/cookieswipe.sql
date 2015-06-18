c93a366c2b753bd6dd105f41d82dbaaa4c11cbf3
Mickaël Lafages <mickx971.l@gmail.com>
2015-06-19 00:11:27 +0200

    Modification du script de création de base. Ajout du protocole de stockage de les domaines

--- resources/sql/cookieswipe.sql
+++ resources/sql/cookieswipe.sql
@@ -1,39 +1,38 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
 --
--- Client :  127.0.0.1
--- Généré le :  Mer 03 Juin 2015 à 11:51
--- Version du serveur :  5.6.17-log
--- Version de PHP :  5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: cookieswipe
+-- ------------------------------------------------------
+-- Server version	5.6.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données :  `cookieswipe`
+-- Table structure for table `domain`
 --
 
-CREATE DATABASE IF NOT EXISTS `cookieswipe`
+DROP DATABASE IF EXISTS cookieswipe ;
+
+CREATE DATABASE IF NOT EXISTS cookieswipe 
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci ;
 
 
-USE `cookieswipe`;
+USE cookieswipe;
 
--- --------------------------------------------------------
 
---
--- Structure de la table `domain`
---
-
-CREATE TABLE IF NOT EXISTS `domain` (
+DROP TABLE IF EXISTS `domain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domain` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `adresse` varchar(45) NOT NULL,
@@ -41,26 +40,29 @@ CREATE TABLE IF NOT EXISTS `domain` (
   `smtpaddr` varchar(255) NOT NULL,
   `portin` int(11) NOT NULL,
   `portout` int(11) NOT NULL,
+  `protocol` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contenu de la table `domain`
+-- Dumping data for table `domain`
 --
 
-INSERT INTO `domain` (`id`, `name`, `adresse`, `popaddr`, `smtpaddr`, `portin`, `portout`) VALUES
-(1, 'Yahoo', 'yahoo.fr', 'pop.mail.yahoo.fr', 'smtp.mail.yahoo.fr', 995, 465),
-(2, 'Microsoft', 'hotmail.com / hotmail.fr', 'pop3.live.com', 'smtp.live.com', 995, 587),
-(3, 'Orange', 'orange.fr / wanadoo.fr', 'imap.free.fr', 'smtp.orange.fr', 995, 465),
-(4, 'Google', 'gmail.com', 'pop.gmail.com', 'smtp.gmail.com', 993, 465);
-
--- --------------------------------------------------------
+LOCK TABLES `domain` WRITE;
+/*!40000 ALTER TABLE `domain` DISABLE KEYS */;
+INSERT INTO `domain` VALUES (1,'Yahoo','yahoo.fr','pop.mail.yahoo.fr','smtp.mail.yahoo.fr',995,465,'pop3'),(2,'Microsoft','hotmail.com / hotmail.fr','pop3.live.com','smtp.live.com',995,587,'pop3'),(3,'Orange','orange.fr / wanadoo.fr','imap.free.fr','smtp.orange.fr',995,465,NULL),(4,'Google','gmail.com','pop.gmail.com','smtp.gmail.com',993,465,'imaps');
+/*!40000 ALTER TABLE `domain` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `mailaccount`
+-- Table structure for table `mailaccount`
 --
 
-CREATE TABLE IF NOT EXISTS `mailaccount` (
+DROP TABLE IF EXISTS `mailaccount`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mailaccount` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `adresse` varchar(255) NOT NULL,
@@ -73,51 +75,55 @@ CREATE TABLE IF NOT EXISTS `mailaccount` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `id_idx` (`user_id`),
-  KEY `domain_mailaccount_idx` (`domain`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+  KEY `domain_mailaccount_idx` (`domain`),
+  CONSTRAINT `domain_mailaccount` FOREIGN KEY (`domain`) REFERENCES `domain` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_mailaccount` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contenu de la table `mailaccount`
+-- Dumping data for table `mailaccount`
 --
 
-INSERT INTO `mailaccount` (`id`, `user_id`, `adresse`, `cookieswipename`, `domain`, `password`, `lastsync`, `mailsignature`, `color`) VALUES
-(7, 1, 'panda.roux.corp@gmail.com', 'panda.roux.corp@gmail.com', 4, 'm65ZYlpIK6VKM9ZEBJZNdA==', NULL, NULL, 'Jaune'),
-(9, 1, 'lucas.girardin@outlook.com', 'lucas.girardin', 2, 'FKU6zn6Riws0ijpkYWAXDg==', NULL, NULL, 'Rouge'),
-(10, 1, 'girardin.lucas@gmail.com', 'girardin.lucas', 4, 'FBeHLRY40qCD6EDNdU5Q3Q==', NULL, NULL, 'Bleu');
-
--- --------------------------------------------------------
+LOCK TABLES `mailaccount` WRITE;
+/*!40000 ALTER TABLE `mailaccount` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mailaccount` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `backupAdr` varchar(255) DEFAULT NULL,
   `blacklist` longtext,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contenu de la table `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `login`, `password`, `backupAdr`, `blacklist`) VALUES
-(1, 'panda.roux.corp@gmail.com', 'H8mxSuAXrWJst9TuxCLXWw==', 'lucas.girardin@outlooK.com', '');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'panda.roux.corp@gmail.com','ftkQGFdCHrYow/XnlXFK1Q==','lucas.girardin@outlooK.com','');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Contraintes pour les tables exportées
---
-
---
--- Contraintes pour la table `mailaccount`
---
-ALTER TABLE `mailaccount`
-  ADD CONSTRAINT `domain_mailaccount` FOREIGN KEY (`domain`) REFERENCES `domain` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `user_mailaccount` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-06-19  0:04:44
