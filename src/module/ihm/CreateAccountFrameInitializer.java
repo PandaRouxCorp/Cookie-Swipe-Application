13859ac68e0c7cf7251a9458b753cf2ab1d03857
Yo911 <yehouda_arnauve@hotmail.com>
2015-07-09 19:46:53 +0200

    cancel inscription

--- src/module/ihm/CreateAccountFrameInitializer.java
+++ src/module/ihm/CreateAccountFrameInitializer.java
@@ -8,6 +8,8 @@ package module.ihm;
 import controller.ActionName;
 import controller.Dispatcher;
 import interfaces.AbstractIHMAction;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import view.component.CookieSwipeButton;
 import view.component.CookieSwipeFrame;
 
@@ -17,17 +19,30 @@ import view.component.CookieSwipeFrame;
  */
 public class CreateAccountFrameInitializer extends AbstractIHMAction{
     
+    
+    private final CookieSwipeFrame frame;
+            
     public CreateAccountFrameInitializer(CookieSwipeFrame csFrame) {
         super(csFrame);
+        frame = csFrame;
         System.out.println("");
     }
     
     @Override
     public boolean execute(Object ... object) {
         Dispatcher dispatcher = new Dispatcher();
-        CookieSwipeButton button = (CookieSwipeButton) hsJcomponent.get("cookieSwipeButtonValidate");
-        button.addActionListener(dispatcher);
-        button.setActionCommand(ActionName.createAccount);
+        CookieSwipeButton validate = (CookieSwipeButton) hsJcomponent.get("cookieSwipeButtonValidate");
+        validate.addActionListener(dispatcher);
+        validate.setActionCommand(ActionName.createAccount);
+        
+        CookieSwipeButton cancel = (CookieSwipeButton) hsJcomponent.get("cookieSwipeButtonCancel");
+        cancel.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
         return true;
     }
 }
