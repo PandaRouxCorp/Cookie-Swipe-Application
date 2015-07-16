1f57a12b2e52f70ee7adb32f6783951392b16669
Suiken <kevin.suy@me.com>
2015-07-16 17:04:41 +0200

    fix refactoring

--- src/view/BlacklistCSFrame.java
+++ src/view/BlacklistCSFrame.java
@@ -107,27 +107,27 @@ public class BlacklistCSFrame extends CookieSwipeFrame implements IJFrame{
 	}
 	
 	public static void main(String args[]) {
-			try {
-			    for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-					if ("Nimbus".equals(info.getName())) {
-					    UIManager.setLookAndFeel(info.getClassName());
-					    break;
-					}
-			    }
-			} catch (ClassNotFoundException ex) {
-			    java.util.logging.Logger.getLogger(MainCSFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-			} catch (InstantiationException ex) {
-			    java.util.logging.Logger.getLogger(MainCSFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-			} catch (IllegalAccessException ex) {
-			    java.util.logging.Logger.getLogger(MainCSFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-			} catch (UnsupportedLookAndFeelException ex) {
-			    java.util.logging.Logger.getLogger(MainCSFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-			}
+		try {
+		    for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+				    UIManager.setLookAndFeel(info.getClassName());
+				    break;
+				}
+		    }
+		} catch (ClassNotFoundException ex) {
+		    java.util.logging.Logger.getLogger(MainCSFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+		    java.util.logging.Logger.getLogger(MainCSFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+		    java.util.logging.Logger.getLogger(MainCSFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (UnsupportedLookAndFeelException ex) {
+		    java.util.logging.Logger.getLogger(MainCSFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		}
 
-			java.awt.EventQueue.invokeLater(new Runnable() {
-			    public void run() {
-			    	new BlacklistCSFrame().setVisible(true);
-			    }
-			});
-	    }
+		java.awt.EventQueue.invokeLater(new Runnable() {
+		    public void run() {
+		    	new BlacklistCSFrame().setVisible(true);
+		    }
+		});
+    }
 }
